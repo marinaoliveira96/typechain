46d1b568e83005751fbaee924c01a9037800ad6f
Marina <marina09oliveira@gmail.com>
2020-12-26 22:56:45 -0300

    classes

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
-interface Human {
-  name: string,
-  age: number,
-  gender: string
+class Human {
+  // javascript n tem private e public
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender:string){
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
-const person = {
-  name: "Marina",
-  age: 24,
-  gender: 'female',
-}
+
+const lynn = new Human("Lynn", 18, "female")
+
 const sayHi = (person: Human): string => {
   return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`
 }
-console.log(sayHi(person))
+console.log(sayHi(lynn))
 
 export {}
