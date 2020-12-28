171e8d174fa4c4b0a1dc66f5ca299f7884a1574a
Marina <marina09oliveira@gmail.com>
2020-12-28 11:52:04 -0300

    create block finished

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import * as CryptoJs from 'crypto-js'
+import * as CryptoJS from "crypto-js";
 
 class Block {
   public index: number;
@@ -6,33 +6,61 @@ class Block {
   public previousHash: string;
   public data: string;
   public timestamp: number;
-  //  disponível fora 
-  static calculateBlockHash = (index: number, previousHash: string, timestamp: number, data: string): string => CryptoJs.SHM256(index+previousHash + timestamp + data).toString() 
+
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 
   constructor(
     index: number,
     hash: string,
     previousHash: string,
     data: string,
-    timestamp: number,
- ){
+    timestamp: number
+  ) {
     this.index = index;
-    this.hash= hash;
+    this.hash = hash;
     this.previousHash = previousHash;
     this.data = data;
-    this.timestamp = timestamp
+    this.timestamp = timestamp;
   }
 }
-const genesisBlock: Block = new Block(0, "202020202020", "", "Hello", 123456);
+
+const genesisBlock: Block = new Block(0, "2020202020202", "", "Hello", 123456);
 
 let blockchain: Block[] = [genesisBlock];
 
-const getBlockchain = () : Block[] => blockchain;
+const getBlockchain = (): Block[] => blockchain;
 
-const getLatesBlockchain = () : Block => blockchain[blockchain.length -1];
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
 
-const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000)
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
-console.log(blockchain)
+const createNewBlock = (data: string): Block => {
+  const previosBlock: Block = getLatestBlock();
+  const newIndex: number = previosBlock.index + 1;
+  const newTimestamp: number = getNewTimeStamp();
+  const newHash: string = Block.calculateBlockHash(
+    newIndex,
+    previosBlock.hash,
+    newTimestamp,
+    data
+  );
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previosBlock.hash,
+    data,
+    newTimestamp
+  );
+  return newBlock;
+};
 
-export {}
+console.log(createNewBlock("hi hi"));
+console.log(createNewBlock("bye bye"));
+
+export {};
